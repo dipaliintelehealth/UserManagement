f43b042c7aeda03f8455303eda7b6a2e3af7a61c
Dipali Nagtilak <dipali@intelehealth.org>
2021-09-14 13:40:34 +1000

    IsActive for md_login set as 1

--- UserManagement.Business/Services/MemberBulkDataImportService.cs
+++ UserManagement.Business/Services/MemberBulkDataImportService.cs
@@ -487,7 +487,7 @@ namespace UserManagement.Business
                 UserName = x.Model.UserName,
                 Password = "ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413",
                 ReferenceId = x.Model.MemberId,
-                IsActive = "",
+                IsActive = 1,
                 SourceId = "99"
             });
 
--- UserManagement.Domain/LoginModel.cs
+++ UserManagement.Domain/LoginModel.cs
@@ -13,7 +13,7 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public string ReferenceId { get; set; }
-        public string IsActive { get; set; }
+        public int IsActive { get; set; }
         public string SourceId { get; set; }
     }
 }
